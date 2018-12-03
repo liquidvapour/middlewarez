3bd76192dc307607e77fc6a1b8ccec110edff773
Ra-el Peters <ra-el.peters@tesco.com>
2018-12-03 13:54:43 +0000

    initial commit

---
+++ src/pipeline.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function wrap(thingToWrap) {
+    const items = [thingToWrap];
+
+    async function call(event, context) {
+        const next = async () => {
+            const func = items.pop();
+            return await func(event, context, next);
+        };
+
+        return await next();
+    }
+
+    function use(thingToUse) {
+        items.push(thingToUse);
+        return this;
+    }
+
+    call.use = use;
+
+    return call;
+}
+
+module.exports = { wrap };
---
+++ tests/integration.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const { wrap } = require("../src/pipeline");
+
+async function heart(event, context) {
+    console.log("in the middle Cirel");
+    console.log(`event: ${event}`);
+    console.log(`context ${context}`);
+
+    return { statusCode : 200 };
+}
+
+async function first(event, context, next) {
+    console.log("do this first");
+    const result = await next();
+    console.log("do this after");
+    return result;
+}
+
+async function second(event, context, next) {
+    console.log("do this before second");
+    const result = await next();
+    console.log("do this after second");
+    return result;
+}
+
+const chain = wrap(heart).use(second).use(first);
+
+(async () => {
+    const result = await chain({ verb: "GET"}, { day: "monday"})
+    console.log(result);
+})();
